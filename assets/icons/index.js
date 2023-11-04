fcf6c651c3b0befac98ee45f4d519a5fec0e5b00
Afochar <aflowchars@gmail.com>
2023-11-04 20:01:09 +0700

    first commit

---
+++ assets/icons/index.js
@@ -0,0 +1,21 @@
+import Dialog2LineIcon from "./Dialog2LineIcon.vue";
+import FacebookLineIcon from "./FacebookLineIcon.vue";
+import FiltersCircleLineIcon from "./FiltersCircleLineIcon.vue";
+import InstagramLineIcon from "./InstagramLineIcon.vue";
+import LinkedinLineIcon from "./LinkedinLineIcon.vue";
+import MapLineIcon from "./MapLineIcon.vue";
+import MasksLineIcon from "./MasksLineIcon.vue";
+import SmileCircleLineIcon from "./SmileCircleLineIcon.vue";
+import YoutubeLineIcon from "./YoutubeLineIcon.vue";
+
+export {
+  Dialog2LineIcon,
+  FacebookLineIcon,
+  FiltersCircleLineIcon,
+  InstagramLineIcon,
+  LinkedinLineIcon,
+  MapLineIcon,
+  MasksLineIcon,
+  SmileCircleLineIcon,
+  YoutubeLineIcon,
+};
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ["Nunito Sans", "sans-serif"],
+      },
+      boxShadow: {
+        1: "0px 2px 6px 0px rgba(208, 208, 208, 0.25)",
+      },
+      colors: {
+        baseline: {
+          primary: "#F0001F",
+        },
+        grey: {
+          barrier: "#EBEDEF",
+          black: "#414853",
+          concencrated: "#212427",
+          footer: "#F6F7F8",
+          "secondary-5": "#CED2D8",
+          smooth: "#586479",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
